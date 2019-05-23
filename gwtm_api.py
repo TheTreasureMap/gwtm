75d68ba988bdf655ecfcd7f5a74937fe92c76d88
Samuel <swyatt@email.arizona.edu>
2019-05-22 17:05:16 -0700

    API testing

--- gwtm_api.py
+++ gwtm_api.py
@@ -12,17 +12,13 @@ from __init__ import app
 
 db = models.db
 
-#from osgeo import ogr
-
 #API Endpoints
-
 
 #Post Pointing/s
 #Parameters: List of Pointing JSON objects
 #Returns: List of assigned IDs
 #Comments: Check if instrument configuration already exists to avoid duplication. 
 #Check if pointing is centered at a galaxy in one of the catalogs and if so, associate it.
-
 
 @app.route("/pointings", methods=["POST"])
 def add_pointings():
@@ -66,7 +62,7 @@ def add_pointings():
 	db.session.flush()
 	db.session.commit()
 
-	return jsonify([x.json for x in points])
+	return jsonify([x.id for x in points])
 
 #Get Pointing/s
 #Parameters: List of ID/s, type/s, group/s, user/s, and/or time/s constraints (to be AND’ed). 
@@ -78,25 +74,27 @@ def get_pointings():
 	args = request.args
 
 	filter=[]
-	gwconstraints = []
+
 	if "graceid" in args:
 		#validate
 		graceid = args.get('graceid')
 		filter.append(models.pointing_event.graceid == graceid)
 		filter.append(models.pointing_event.pointingid == models.pointing.id)
+
 	if "id" in args:
 		#validate
 		_id = args.get('id')
 		filter.append(models.pointing.id == int(_id))
-	if "ids" in args:
+	elif "ids" in args:
 		#validate
 		ids = json.loads(args.get('ids'))
 		filter.append(models.pointing.id.in_(ids))
+
 	if "status" in args:
 		#validate
 		status = args.get('status')
 		filter.append(models.pointing.status == status)
-	if "statuses" in args:
+	elif "statuses" in args:
 		#validate
 		statuses = []
 		statuses_sent = args.get('statuses')
@@ -107,23 +105,89 @@ def get_pointings():
 		if "cancelled" in statuses_sent:
 			statuses.append(models.pointing_status.cancelled)
 		filter.append(models.pointing.status.in_(statuses))
-	if "completed_after" in args:
-		pass
-	if "completed_before" in args:
-		pass
-	if "planned_after" in args:
-		pass
-	if "planned_before" in args:
-		pass
-	if "group" in args:
-		pass
-	if "groups" in args:
-		pass
-	if "user" in args:
-		pass
-	if "users" in args:
-		pass
 
+	if "completed_after" in args:
+		time = args.get('completed_after')
+		try:
+			time = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
+		except:
+			return jsonify("Error parsing date. Should be %Y-%m-%dT%H:%M:%S format. e.g. 2019-05-01T12:00:00")
+		filter.append(models.pointing.status == models.pointing_status.completed)
+		filter.append(models.pointing.time >= time)
+
+	if "completed_before" in args:
+		time = args.get('completed_before')
+		try:
+			time = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
+		except:
+			return jsonify("Error parsing date. Should be %Y-%m-%dT%H:%M:%S format. e.g. 2019-05-01T12:00:00")
+		filter.append(models.pointing.status == models.pointing_status.completed)
+		filter.append(models.pointing.time <= time)
+
+	if "planned_after" in args:
+		time = args.get('planned_after')
+		try:
+			time = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
+		except:
+			return jsonify("Error parsing date. Should be %Y-%m-%dT%H:%M:%S format. e.g. 2019-05-01T12:00:00")
+		filter.append(models.pointing.status == models.pointing_status.planned)
+		filter.append(models.pointing.time >= time)
+
+	if "planned_before" in args:
+		time = args.get('planned_before')
+		try:
+			time = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
+		except:
+			return jsonify("Error parsing date. Should be %Y-%m-%dT%H:%M:%S format. e.g. 2019-05-01T12:00:00")
+		filter.append(models.pointing.status == models.pointing_status.planned)
+		filter.append(models.pointing.time <= time)
+
+	if "group" in args:
+		group = args.get('group')
+		if group.isdigit():
+			filter.append(models.usergroups.groupid == group)
+		else:
+			filter.append(models.groups.name.contains(group))
+			filter.append(models.usergroups.groupid == models.groups.id)
+
+		filter.append(models.usergroups.userid == models.users.id)
+		filter.append(models.users.id == models.pointing.submitterid)
+
+	elif "groups" in args:
+		try:
+			groups = json.loads(args.get('groups'))
+			filter.append(models.usergroups.groupid.in_(groups))
+		except:
+			groups = args.get('groups')
+			groups = groups.split('[')[1].split(']')[0].split(',') 
+			for g in groups:
+				filter.append(models.group.name.contains(g))
+		filter.append(models.usergroups.userid == models.users.id)
+		filter.append(models.users.id == models.pointing.submitterid)
+
+	if "user" in args:
+		user = args.get('user')
+		if user.isdigit():
+			filter.append(models.pointing.submitterid == int(user))
+		else:
+			filter.append(or_(models.users.username.contains(user),
+							  models.users.firstname.contains(user),
+							  models.users.lastname.contains(user)))
+			filter.append(models.users.id == models.pointing.submitterid)
+
+	elif "users" in args:
+		try:
+			users = json.loads(args.get('users'))
+			filter.append(models.pointing.submitterid.in_(user))
+		except:
+			users = args.get('users')
+			users = users.split('[')[1].split(']')[0].split(',') 
+			for u in users:
+				filter.append(or_(models.users.username.contains(u.strip()),
+							  models.users.firstname.contains(u.strip()),
+							  models.users.lastname.contains(u.strip())))
+				filter.append(models.users.id == models.pointing.submitterid)
+		
 	pointings = db.session.query(models.pointing).filter(*filter).all()
 	pointings = [x.json for x in pointings]
 
@@ -152,10 +216,6 @@ def del_pointings():
 	else:
 		return jsonify("Please Don't delete the ENTIRE table")
 
-#Get Instrument/s
-#Parameters: List of ID/s, type/s (to be AND’ed).
-#Returns: List of Instrument JSON objects
-
 
 @app.route("/instruments", methods=["POST"])
 def post_instruments():
@@ -175,7 +235,12 @@ def post_instruments():
 	db.session.flush()
 	db.session.commit()
 
-	return inst.json
+	return inst.id
+
+
+#Get Instrument/s
+#Parameters: List of ID/s, type/s (to be AND’ed).
+#Returns: List of Instrument JSON objects
 
 @app.route("/instruments", methods=["GET"])
 def get_instruments():
