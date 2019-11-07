8b42e6cf06f66ec4b7d71e9985171103069acf51
tohuvavohu <aaron.tohu@gmail.com>
2019-11-07 02:50:32 -0500

    add suport for quick GBM coverage

--- src/function.py
+++ src/function.py
@@ -1,7 +1,10 @@
 from numpy import genfromtxt
 import numpy as np
-
+import ephem
+from urllib.request import urlopen
 import math
+from bs4 import BeautifulSoup
+
 
 def readconfig(directory,_file):
     data=genfromtxt(directory+_file,str)
@@ -198,3 +201,109 @@ def project_footprint(footprint, ra, dec, pos_angle):
         pt_ra, pt_dec = uvec_to_ra_dec(new_x, new_y, new_z)
         proj_footprint.append([pt_ra, pt_dec])
     return proj_footprint
+
+def getDataFromTLE(datetime, tleLatOffset=0, tleLonOffset=0.21):
+    # Get TLE and parse
+    url = "https://celestrak.com/satcat/tle.php?CATNR=33053"
+    data = urlopen(url)
+    tle_raw=data.read()
+    clean_tle = ''.join(BeautifulSoup(tle_raw, "html.parser").stripped_strings)
+    tle_obj = clean_tle.split('\r\n')
+
+    # Print age of TLE
+    year = "20"+tle_obj[1][18:20]
+    day = tle_obj[1][20:32]
+    #print("\nTLE most recently updated "+year+"DOY"+ day)
+    
+    # Create spacecraft instance
+    Fermi = ephem.readtle(tle_obj[0],tle_obj[1],tle_obj[2])
+    
+    # Create observer
+    observer_Fermi = ephem.Observer()
+    observer_Fermi.lat = '0'
+    observer_Fermi.long = '0'
+    
+    # Initialize lists
+    lat = []
+    lon = []
+    elevation = []
+    
+    # Iterate through time; compute predicted locations
+    observer_Fermi.date = ephem.date(datetime)
+    Fermi.compute(observer_Fermi)
+    lat.append(np.degrees(Fermi.sublat.znorm))
+    lon.append(np.degrees(Fermi.sublong.norm))
+    elevation.append(Fermi.elevation)
+    # Correct for inaccuracy
+    lon = np.array(lon) - tleLonOffset
+    lat = np.array(lat) - tleLatOffset
+    
+    if len(lon) == 1: # Return single value instead of array if length is 1
+        lon = lon[0]
+        lat = lat[0]
+        elevation = elevation[0]
+    
+    return lon, lat, elevation
+
+# convert degrees to deg:amin:asec
+def deg2dm(deg):
+    sign = np.sign(deg)
+    deg = np.abs(deg)
+    d = np.floor(deg)
+    m = (deg - d) * 60
+    return int(sign*d), m
+
+def getGeoCenter(datetime, lon, lat):
+    # Define the observer to be at the location of the spacecraft
+    observer = ephem.Observer()
+
+    # Convert the longitude to +E (-180 to 180)
+    if lon > 180:
+       lon = (lon % 180) - 180
+
+    lon_deg, lon_min = deg2dm(lon)
+    lat_deg, lat_min = deg2dm(lat)
+
+    lon_string = '%s:%s' % (lon_deg, lon_min)
+    lat_string = '%s:%s' % (lat_deg, lat_min)
+
+    observer.lon = lon_string
+    observer.lat = lat_string
+    
+    # Set the time of the observations
+    observer.date = ephem.date(datetime)
+    
+    # Get the ra and dec (in radians) of the point in the sky at altitude = 90 (directly overhead)
+    ra_zenith_radians, dec_zenith_radians = observer.radec_of('0', '90')
+    
+    # convert the ra and dec to degrees
+    ra_zenith = np.degrees(ra_zenith_radians)
+    dec_zenith = np.degrees(dec_zenith_radians)
+    
+    ra_geocenter =  (ra_zenith+180) % 360
+    dec_geocenter = -1 * dec_zenith
+    
+    return ra_geocenter, dec_geocenter
+    
+def getearthsatpos(datetime):
+    tleLonOffset=0.21
+    tleLatOffset = 0
+
+    lon, lat, elevation= getDataFromTLE(datetime, tleLatOffset=tleLatOffset, tleLonOffset=tleLonOffset)
+
+    # Get the geo center coordinates in ra and dec
+    ra_geocenter, dec_geocenter = getGeoCenter(datetime, lon, lat)
+
+    EARTH_RADIUS = 6378.140 * 1000 #in meters
+    dtor = math.pi/180
+    elev = elevation + EARTH_RADIUS
+    earthsize_rad = np.arcsin(EARTH_RADIUS/elev)/dtor
+
+    return ra_geocenter,dec_geocenter, earthsize_rad
+
+def makeEarthContour(ra,dec,radius):
+    thetas = np.linspace(0, -2*np.pi, 200)
+    ras = radius * np.cos(thetas) + ra
+    decs = radius * np.sin(thetas) + dec
+    contour = np.c_[ras,decs]
+    return contour
