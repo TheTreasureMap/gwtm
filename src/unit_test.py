59a323af49d0f76daf28ef50a78acc7b0d8eaebc
achu123 <achu123@uw.edu>
2023-05-08 15:48:10 -0700

    Unit Test clean up and added more get requests

--- src/unit_test.py
+++ src/unit_test.py
@@ -18,57 +18,97 @@ import datetime
 class TestAPI(unittest.TestCase):
   BASE = 'http://127.0.0.1:5000/api/v1' # Base address
   API_TOKEN = "Tsqx2GKTa7BdFMSrpqYWIOHtrI3cnccZeC0_LQ" # API token
-  TARGET_1 = "pointings"
-  TARGET_2 = "footprints"
-  TARGET_3 = "event_galaxies"
+  TARGET_POINTINGS = "pointings"
+  TARGET_FOOTPRINTS = "footprints"
+  TARGET_EVENT_GALAXIES = "event_galaxies"
+  TARGET_GLADE = "glade"
+  TARGET_INSTRUMENTS = "instruments"
   INSTRUMENT_NAME = "Sinistro"
   INSTRUMENT_ID = 9
   GRACE_ID = "GW190814"
 
   ### GET Method tests
-  def test_pointings_1(self):
+
+  # @JSON parameters: graceid, graceids, band, bands, status, completed_after, completed_before, user, users,
+  # instrument, instruments, wavelength, frequency and energy regimes
+  def test_pointings(self):
     json_params = {
         "api_token": self.API_TOKEN,
         "band":"XRT",
         "status":"completed",
         "graceid": self.GRACE_ID
       }
-    url = "{}/{}".format(self.BASE, self.TARGET_1)
+    url = "{}/{}".format(self.BASE, self.TARGET_POINTINGS)
     r = requests.get(url=url, json=json_params)
     self.assertEqual(r.status_code, 200)
     #self.assertEqual(len(r.json()), 483)
     print("Test 1 [Pointings for " + self.GRACE_ID + " event] passed")
     print(r.text)
 
-  def test_footprints_1(self):
+  # @JSON parameters: id, name
+  def test_footprints(self):
     json_params = {
         "api_token": self.API_TOKEN,
         "id" : self.INSTRUMENT_ID,
         "name" : self.INSTRUMENT_NAME
       }
-    url = "{}/{}".format(self.BASE, self.TARGET_2)
+    url = "{}/{}".format(self.BASE, self.TARGET_FOOTPRINTS)
     r = requests.get(url=url, json=json_params)
     self.assertEqual(r.status_code, 200)
     print("Test 1 [Footprints for " + self.GRACE_ID + " event] passed")
     print(r.text)
 
-  def test_event_galaxy_1(self):
+  # @JSON parameters: graceid, timesent_stamp, listid, groupname, score_gt, score_lt
+  def test_event_galaxy(self):
     json_params = {
         "api_token": self.API_TOKEN,
         "id" : self.INSTRUMENT_ID,
         "name" : self.INSTRUMENT_NAME
       }
-    url = "{}/{}".format(self.BASE, self.TARGET_3)
+    url = "{}/{}".format(self.BASE, self.TARGET_EVENT_GALAXIES)
     r = requests.get(url=url, json=json_params)
     #self.assertEqual(r.status_code, 200)
     print("Test 1 [Event Galaxies for " + self.GRACE_ID + " event] passed")
     print(r.text)
 
+  # @JSON parameters: RA, DEC, name
+  def test_glade(self):
+    json_params = {
+        "api_token": self.API_TOKEN,
+        "id" : self.INSTRUMENT_ID,
+        #ra
+        #dec
+        "name" : self.INSTRUMENT_NAME
+      }
+    url = "{}/{}".format(self.BASE, self.TARGET_GLADE)
+    r = requests.get(url=url, json=json_params)
+    #self.assertEqual(r.status_code, 200)
+    print("Test 1 [Event Galaxies for " + self.GRACE_ID + " event] passed")
+    print(r.text)
+
+  # @JSON parameters: id, ids, name, names
+  def test_instrument(self):
+    json_params = {
+        "api_token": self.API_TOKEN,
+        "id" : self.INSTRUMENT_ID,
+        "name" : self.INSTRUMENT_NAME
+        #ids
+        #names
+      }
+    url = "{}/{}".format(self.BASE, self.TARGET_INSTRUMENTs)
+    r = requests.get(url=url, json=json_params)
+    #self.assertEqual(r.status_code, 200)
+    print("Test 1 [Event Galaxies for " + self.GRACE_ID + " event] passed")
+    print(r.text)
+
+
   ### POST Method tests
 
-
 if __name__ == "__main__":
+
   tester = TestAPI()
-  tester.test_pointings_1()
-  tester.test_footprints_1()
-  tester.test_even_galaxy_1()
+  tester.test_pointings()
+  tester.test_footprints()
+  tester.test_event_galaxy()
+  tester.test_glade()
+  tester.test_instrument()
