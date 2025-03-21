c0d3f64d6e8f7bb7e5626682e12b979328a07e17
Chris Ni <cni@lco.global>
2025-03-20 18:42:05 -0700

    Fixes instrument overlay toggling issue (#74) for same-named instruments.

--- src/static/js/alert_aladin.js
+++ src/static/js/alert_aladin.js
@@ -217,7 +217,9 @@ function aladin_removeContour(
 }
 
 /*
-    Hides or shows an overlay from the checkbox
+    Hides or shows an overlay from the checkbox.
+    Checks color match: two insts should never have the same color,
+    but sometimes two insts (same network) can have the same name.
 */
 function aladin_overlayToggleOne(
     target,
@@ -226,7 +228,7 @@ function aladin_overlayToggleOne(
     var iter = 0
     for(var k=0; k<overlay_list.length; k++)
     {
-        if (target.id == overlay_list[k].contour.name) {
+        if (target.dataset.color == overlay_list[k].contour.color) {
             iter = k
         }
     }
@@ -378,7 +380,7 @@ function aladin_drawInstHTML(
             <li>\
                 <fieldset>\
                     <label for="' + cat.name + '" style="display: inline-block;">\
-                        <input id="' + cat.name + '" type="checkbox" value="' + cat.name + '" checked="checked" style="display: inline-block;"> \
+                        <input id="' + cat.name + '" type="checkbox" value="' + cat.name + '" data-color="' + cat.color + '" checked="checked" style="display: inline-block;"> \
                             <div class="overlaycolorbox" style="background-color: '+cat.color+';"></div>\
                             <span> '+cat.name+'</span>\
                         </input>\
