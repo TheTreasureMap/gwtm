14e1b3083cf0c0f532af716e14e7ed9eae3661ee
Steve Foale <sfoale@lco.global>
2025-09-11 15:08:07 +0100

    Allow all hosts in dev mode.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 		host: '0.0.0.0',
 		port: 3000,
 		strictPort: true,
-		allowedHosts: 'all', // Allow all hosts in development
+		allowedHosts: true,
 		hmr: {
 			port: 3000
 		},
